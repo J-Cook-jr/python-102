1633966b471aabf9ab7c7dbf7b3b2c9776985027
Jeff Cook <jecook2692@gmail.com>
2020-04-02 00:30:17 -0400

    adds farenheit file

---
+++ exercise6.py
@@ -0,0 +1,17 @@
+#1. Prompt user for temp. in celsius
+celsius = input('Temperature in c? ')
+
+#2. Calculates the user's input
+celsius = int(celsius)* 9 / 5 + 32
+
+#3. Converts celsius to Farenheit
+farenheit = celsius
+
+#4. prints the result of celsius to farenheit calculation
+print (farenheit)
+
+
+
+
+
+
